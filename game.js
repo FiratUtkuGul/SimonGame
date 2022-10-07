872abb5b53a6868e073245800d6d5784c602e588
Fırat Utku <firatutkugul01@outlook.com>
2022-10-07 09:02:21 +0300

    Organising Code

--- game.js
+++ game.js
@@ -1,55 +1,4 @@
-// //Variables
-// var buttonColours = ["red","blue","green","yellow"];
-// var gamePattern = [];
-// var userChosenPattern = [];
-// var level = 0;
-//
-//
-// function nextSequence(){
-//   //Incresing the Level
-//   level++;
-//   $("h1").text("Level " + level);
-//
-//   //Selecting a random colour using a random number and pushing it
-//   var randomNumber = Math.floor(Math.random()*4);
-//   var randomChosenColour = buttonColours[randomNumber];
-//   gamePattern.push(randomChosenColour);
-//
-//   $("#" + randomChosenColour).on("click", function(){
-//     $("." + randomChosenColour).fadeOut(100).fadeIn(100);
-//     playSound(randomChosenColour);
-//   });
-// };
-//
-// //Whenever a button is clicked plays the corresponding sound and animates the button
-// $(".btn").click(function(event) {
-//   nextSequence();
-//   var userChosenColour = $(this).attr("id");
-//   userChosenPattern.push(userChosenColour);
-//   playSound($(this).attr("id"));
-//   animatePress($(this).attr("id"));
-// })
-//
-// //Plays the sound related to the corresponding button
-// function playSound(name){
-//   var audio = new Audio("sounds/" + name + ".mp3");
-//   audio.play();
-// }
-//
-// //Animates the button when pressed
-// function animatePress(currentColour){
-//   $("#" + currentColour).addClass("pressed");
-//   myTimeout = setTimeout(function() {
-//     $("#" + currentColour).removeClass("pressed");
-//   }, 100);
-// }
-//
-// $(document).keypress(function() {
-//   if(level == 0 && ){
-//     nextSequence();
-//     $("h1").text("Level " + level);
-//   }
-// });
+//Variables
 var buttonColours = ["red","blue","green","yellow"];
 var gamePattern = [];
 var userChosenPattern = [];
@@ -58,20 +7,27 @@ var isGameOver = false;
 var checkAnswerIndex = 0;
 
 function nextSequence() {
+  //Incresing the Level
+  level++;
+  $("h1").text("Level " + level);
+
+  //Selecting a random colour using a random number and pushing it
   var randomNumber = Math.floor(Math.random()*4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-  level++;
-  $("h1").text("Level " + level);
+
+  //Flashing the random button
   $("." + randomChosenColour).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
 }
 
+//Plays the sound related to the corresponding button
 function playSound(name){
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
+//Animates and checks the user answer of the pressed button
 $(".btn").click(function() {
    var userChosenColour = $(this).attr("id");
    userChosenPattern.push(userChosenColour);
@@ -80,6 +36,7 @@ $(".btn").click(function() {
    checkAnswer(userChosenColour);
 })
 
+//Animates the button when pressed
 function animatePress(currentColour){
    $("#" + currentColour).addClass("pressed");
    myTimeout = setTimeout(function() {
@@ -87,6 +44,7 @@ function animatePress(currentColour){
    }, 100);
 }
 
+//Controls the initial game start and starting over
 $(document).keydown(function(event) {
    if(level == 0 && event.key == "a"){
      myTimeout = setTimeout(function() {
@@ -99,6 +57,7 @@ $(document).keydown(function(event) {
    }
  });
 
+//Checks the user pattern to the game pattern
 function checkAnswer(userColour) {
   if(userColour == gamePattern[checkAnswerIndex] && !isGameOver){
     checkAnswerIndex++;
@@ -122,6 +81,7 @@ function checkAnswer(userColour) {
   }
 }
 
+//Resets the game variables
 function startOver(){
   level = 0;
   gamePattern = [];
